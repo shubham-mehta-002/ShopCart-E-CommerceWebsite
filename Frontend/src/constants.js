db0dc875886d6c39ce0d4c39eed47b98946ef781
shubham-mehta-002 <mehta.shubham002@gmail.com>
2024-08-18 00:08:08 +0530

    add backend url

--- Frontend/src/constants.js
+++ Frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:4000";
-// export const BASE_URL = "https://shop-cart-ehmw.onrender.com/"
+// export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://shop-mart-4f57.onrender.com/"
 export const ITEMS_PER_PAGE = 15
