d36e1725cdd0183152abaa4632c65c946bcb2389
shubham-mehta-002 <mehta.shubham002@gmail.com>
2024-08-18 12:00:56 +0530

    a

--- Frontend/src/Features/Auth/Components/ProtectedRoute.jsx
+++ Frontend/src/Features/Auth/Components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 export function ProtectedRoute({ children }) {
 
-
+  console.log(Cookies)
   if (!Cookies.get("loggedInUserInfo")) {
       console.log("DA")
     return <Navigate to="/login"></Navigate>;
