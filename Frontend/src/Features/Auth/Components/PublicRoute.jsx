fe06a896f178d92b463e83a1a35eb94ab32681fc
Shubham Mehta <134730928+shubham-mehta-002@users.noreply.github.com>
2024-10-30 02:11:56 +0530

    Update PublicRoute.jsx

--- Frontend/src/Features/Auth/Components/PublicRoute.jsx
+++ Frontend/src/Features/Auth/Components/PublicRoute.jsx
@@ -4,7 +4,7 @@ import { selectLoggedInUser } from '../AuthSlice';
 import Cookies from "js-cookie"
 
 export const PublicRoute = ({ children }) => {
-
+console.log("COOKIES",Cookies.get())
     if (Cookies.get("loggedInUserInfo")) {
         return <Navigate to="/"></Navigate>;
     }
